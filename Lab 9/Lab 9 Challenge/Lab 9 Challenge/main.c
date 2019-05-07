66d7e6d20f5cf330b9b7af94fca6c08543cf3522
Candr018 <49248384+Candr018@users.noreply.github.com>
2019-05-07 11:05:11 -0700

    ii

--- Lab 9/Lab 9 Challenge/Lab 9 Challenge/main.c
+++ Lab 9/Lab 9 Challenge/Lab 9 Challenge/main.c
@@ -6,6 +6,7 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 // ---------------------------------------------------- Global Var definitions --------------------------------------------------------------------------------------------//
 enum States {init, wait_start, space_state, wait_release, measure_1, measure_2, measure_3, measure_4, measure_5, measure_6, measure_7, measure_8} next_state;
@@ -35,184 +36,95 @@ unsigned char button_pressed = 0x00; //boolean to keep track of the button press
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 // Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
-
 unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
-
 unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
 
-
-
 void TimerOn() {
-
 	// AVR timer/counter controller register TCCR1
-
 	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
-
 	// bit2bit1bit0=011: pre-scaler /64
-
 	// 00001011: 0x0B
-
 	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
-
 	// Thus, TCNT1 register will count at 125,000 ticks/s
 
-
-
 	// AVR output compare register OCR1A.
-
 	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
-
 	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
-
 	// So when TCNT1 register equals 125,
-
 	// 1 ms has passed. Thus, we compare to 125.
-
 	// AVR timer interrupt mask register
-
 	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
 
-
-
 	//Initialize avr counter
-
 	TCNT1=0;
 
-
-
 	_avr_timer_cntcurr = _avr_timer_M;
-
 	// TimerISR will be called every _avr_timer_cntcurr milliseconds
 
-
-
 	//Enable global interrupts
-
 	SREG |= 0x80; // 0x80: 1000000
-
 }
-
-
 
 void TimerOff() {
-
 	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
-
 }
-
-
 
 void TimerISR() {
-
 	TimerFlag = 1;
-
 }
-
-
 
 // In our approach, the C programmer does not touch this ISR, but rather TimerISR()
-
 ISR(TIMER1_COMPA_vect) {
-
 	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
-
 	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
-
 	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
-
 		TimerISR(); // Call the ISR that the user uses
-
 		_avr_timer_cntcurr = _avr_timer_M;
-
 	}
-
 }
-
-
 
 // Set TimerISR() to tick every M ms
-
 void TimerSet(unsigned long M) {
-
 	_avr_timer_M = M;
-
 	_avr_timer_cntcurr = _avr_timer_M;
-
 }
-
 void set_PWM(double frequency) {
-
 	static double current_frequency; // Keeps track of the currently set frequency
-
 	// Will only update the registers when the frequency changes, otherwise allows
-
 	// music to play uninterrupted.
-
 	if (frequency != current_frequency) {
-
 		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
-
 		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
-
 		
-
-		// prevents OCR3A from overflowing, using pre-scaler 64
-
+		// prevents OCR3A from overflowing, using prescaler 64
 		// 0.954 is smallest frequency that will not result in overflow
-
 		if (frequency < 0.954) { OCR3A = 0xFFFF; }
-
 		
-
-		// prevents OCR0A from underflowing, using pre-scaler 64
-
-		// 31250 is largest frequency that will not result in underflow
-
+		// prevents OCR3A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
 		else if (frequency > 31250) { OCR3A = 0x0000; }
-
 		
-
 		// set OCR3A based on desired frequency
-
 		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
 
-
-
 		TCNT3 = 0; // resets counter
-
 		current_frequency = frequency; // Updates the current frequency
-
 	}
-
 }
-
-
 
 void PWM_on() {
-
 	TCCR3A = (1 << COM3A0);
-
-	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
-
+	// COM3A0: Toggle PB6 on compare match between counter and OCR3A
 	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
-
-	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
-
-	// CS01 & CS30: Set a pre-scaler of 64
-
+	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
+	// CS31 & CS30: Set a prescaler of 64
 	set_PWM(0);
-
 }
-
-
 
 void PWM_off() {
-
 	TCCR3A = 0x00;
-
 	TCCR3B = 0x00;
-
 }
+
 
 // synchronous state machine to run the melody
 void tick() {
@@ -374,6 +286,8 @@ void tick() {
 		// Space state to provide natural flow to music //
 		
 		case space_state:
+		if(counter > 10)
+		{
 		if(cur_state == 0x01) // Conditionals to manage current state and next state
 			{
 				next_state = measure_2;
@@ -410,6 +324,11 @@ void tick() {
 			{
 				next_state = init;
 			}
+		}
+		else
+		{
+			next_state = space_state;
+		}
 		break; 
 		
 		// ------ wait release state to ensure button is released ---- //
@@ -442,67 +361,144 @@ void tick() {
 		
 		case measure_1:
 		input_frequency = G4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x01;
 		break;
 		
 		case measure_2:
 		input_frequency = G4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x02;
 		break;
 		
 		case measure_3:
 		input_frequency = G4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x03;
 		break;
 		
 		case measure_4:
 		input_frequency = F4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x04;
 		break;
 		
 		case measure_5:
 		input_frequency = G4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x05;
 		break;
 		
 		case measure_6:
 		input_frequency = G4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x06;
 		break;
 		
 		case measure_7:
 		input_frequency = G4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x07;
 		break;
 		
 		case measure_8:
 		input_frequency = E4;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		counter++;
 		cur_state = 0x08;
 		break;
 		
 		case space_state:
-		counter = 0x00;
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(counter > 10)
+		{
+			counter = 0x00;
+		}
+		else
+		{
+			counter++;
+		}
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		break;
 		
 		case wait_release:
-		button_pressed = button == 0x00 ? 0x00 : 0x01;
+		if(button == 0x00)
+		{
+			button_pressed = 0x00;
+		}
+		else
+		{
+			button_pressed = 0x01;
+		}
 		break;
 		
 		default:
@@ -514,13 +510,14 @@ void tick() {
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // define port A as inputs
-	DDRB = 0x08; PORTB = 0xF7; // using B3 (0x08) as output to speaker
+	DDRB = 0xFF; PORTB = 0x00; // using B3 (0x08) as output to speaker
 	
 	PWM_on();
-	TimerSet(5);
+	TimerSet(15);
 	TimerOn(); 
 	
 	next_state = init;
+	button_pressed = 0x00;
 
 	while (1)
 	{
--- Lab 9/Lab 9 part 2/Lab 9 part 2/main.c
+++ Lab 9/Lab 9 part 2/Lab 9 part 2/main.c
@@ -8,83 +8,48 @@
 #include <avr/io.h>
 
 void set_PWM(double frequency) {
-
 	static double current_frequency; // Keeps track of the currently set frequency
-
 	// Will only update the registers when the frequency changes, otherwise allows
-
 	// music to play uninterrupted.
-
 	if (frequency != current_frequency) {
-
 		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
-
 		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
-
 		
-
-		// prevents OCR3A from overflowing, using pre-scaler 64
-
+		// prevents OCR3A from overflowing, using prescaler 64
 		// 0.954 is smallest frequency that will not result in overflow
-
 		if (frequency < 0.954) { OCR3A = 0xFFFF; }
-
 		
-
-		// prevents OCR0A from underflowing, using pre-scaler 64
-
-		// 31250 is largest frequency that will not result in underflow
-
+		// prevents OCR3A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
 		else if (frequency > 31250) { OCR3A = 0x0000; }
-
 		
-
 		// set OCR3A based on desired frequency
-
 		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
 
-
-
 		TCNT3 = 0; // resets counter
-
 		current_frequency = frequency; // Updates the current frequency
-
 	}
-
 }
-
-
 
 void PWM_on() {
-
 	TCCR3A = (1 << COM3A0);
-
-	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
-
+	// COM3A0: Toggle PB6 on compare match between counter and OCR3A
 	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
-
-	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
-
-	// CS01 & CS30: Set a pre-scaler of 64
-
+	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
+	// CS31 & CS30: Set a prescaler of 64
 	set_PWM(0);
-
 }
-
-
 
 void PWM_off() {
-
 	TCCR3A = 0x00;
-
 	TCCR3B = 0x00;
-
 }
+
 
 enum States {init, wait_start, wait_input, freq1, freq2, freq3, freq4, freq5, freq6, freq7, freq8} next_state;
 
 double input_frequency;
 unsigned char button;
+unsigned char button_released;
 double C4 = 261.63;
 double D4 = 293.66;
 double E4 = 329.63;
@@ -139,6 +104,8 @@ void tick() {
 		//----------------- Frequency 2 state ----------------------//
 		
 		case freq2:
+		if (button_released == 0x00)
+		{
 		if(button == 0x04)	//increment
 			{
 				next_state = freq3;
@@ -155,11 +122,18 @@ void tick() {
 			{
 				next_state = freq2;
 			}
+		}
+		else
+		{
+			next_state = freq2;
+		}
 		break;
 		
 		//----------------- Frequency 3 state ----------------------//
 		
 		case freq3:
+		if (button_released == 0x00)
+		{
 		if(button == 0x04)	//increment
 			{
 				next_state = freq4;
@@ -176,11 +150,18 @@ void tick() {
 			{
 				next_state = freq3;
 			}
+		}
+		else
+		{
+			next_state = freq3;
+		}
 		break;
 		
 		//----------------- Frequency 4 state ----------------------//
 		
 		case freq4:
+		if (button_released == 0x00)
+		{
 		if(button == 0x04)	// increment
 			{
 				next_state = freq5;
@@ -197,11 +178,18 @@ void tick() {
 			{
 				next_state = freq4;
 			}
+		}
+		else
+		{
+			next_state = freq4;
+		}
 		break;
 		
 		//----------------- Frequency 5 state ----------------------//
 		
 		case freq5:
+		if (button_released == 0x00)
+		{
 		if(button == 0x04)	//increment
 			{
 				next_state = freq6;
@@ -218,11 +206,18 @@ void tick() {
 			{
 				next_state = freq5;
 			}
+		}
+		else
+		{
+			next_state = freq5;
+		}
 		break;
 		
 		//----------------- Frequency 6 state ----------------------//
 		
 		case freq6:
+		if (button_released == 0x00)
+		{
 		if(button == 0x04) // increment
 			{
 				next_state = freq7;
@@ -239,11 +234,18 @@ void tick() {
 			{
 				next_state = freq6;
 			}
+		}
+		else
+		{
+			next_state = freq6;
+		}
 		break;
 		
 		//----------------- Frequency 7 state ----------------------//
 		
 		case freq7:
+		if (button_released == 0x00)
+		{
 		if(button == 0x04) // increment
 		{
 			next_state = freq8;
@@ -259,6 +261,11 @@ void tick() {
 		else // null transition
 		{
 			next_state = freq7;
+		}
+		}
+		else
+		{
+		next_state = freq7;
 		}
 		break;
 		
@@ -301,26 +308,74 @@ void tick() {
 		break;
 		
 		case freq2:
+		if(button == 0x00)
+		{
+			button_released = 0x00;
+		}
+		else
+		{
+			button_released = 0x01;
+		}
 		input_frequency = D4;
 		break;
 		
 		case freq3:
+		if(button == 0x00)
+		{
+			button_released = 0x00;
+		}
+		else
+		{
+			button_released = 0x01;
+		}
 		input_frequency = E4;
 		break;
 		
 		case freq4:
+		if(button == 0x00)
+		{
+			button_released = 0x00;
+		}
+		else
+		{
+			button_released = 0x01;
+		}
 		input_frequency = F4;
 		break;
 		
 		case freq5:
+		if(button == 0x00)
+		{
+			button_released = 0x00;
+		}
+		else
+		{
+			button_released = 0x01;
+		}
 		input_frequency = G4;
 		break;
 		
 		case freq6:
+		if(button == 0x00)
+		{
+			button_released = 0x00;
+		}
+		else
+		{
+			button_released = 0x01;
+		}
 		input_frequency = A4;
 		break;
 		
 		case freq7:
+		if(button == 0x00)
+		{
+			button_released = 0x00;
+		}
+		else
+		{
+			button_released = 0x01;
+		}
 		input_frequency = B4;
 		break;
 		
@@ -336,11 +391,11 @@ void tick() {
 
 int main(void)
 {
-	DDRB = 0x08; PORTB = 0xF7;
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRA = 0x00; PORTA = 0xFF;
 	
 	next_state = init;
-	
+	button_released = 0x00;
 	PWM_on();
 	while (1)
 	{
